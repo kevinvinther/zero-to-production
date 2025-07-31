75f5b9176add7fd34fe94b74a8c572dd6224e850
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-07-31 16:10:04 +0200

    test: add test for health_check

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
-use axum::{Router, http::StatusCode, routing::get};
-use std::net::SocketAddr;
+use axum::{Router, http::StatusCode, routing::get, serve::Serve};
+use std::net::TcpListener;
 
 async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
-pub async fn run() {
+pub fn run(
+    listener: TcpListener,
+) -> Result<Serve<tokio::net::TcpListener, Router, Router>, std::io::Error> {
     let app = Router::new().route("/health_check", get(health_check));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("Listening on http://{addr}");
+    listener.set_nonblocking(true)?;
 
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::from_std(listener)?;
+
+    let server = axum::serve(listener, app);
+
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
-async fn main() {
-    run().await;
+async fn main() -> Result<(), std::io::Error> {
+    let listener =
+        TcpListener::bind("127.0.0.1:0").expect("Failed to bind listener to random port.");
+
+    run(listener)?.await
 }
