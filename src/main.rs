8e46f7f57599d69e16323d74203d1b526ec8ab88
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-07-31 15:19:32 +0200

    feat(axum): add axum and health_check endpoint
    Add axum with boilerplate code to set up a simple server. Furthermore,
    add one route: `health_check`, which returns 200 OK.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{Router, http::StatusCode, routing::get};
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health_check", get(health_check));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("Listening on http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
