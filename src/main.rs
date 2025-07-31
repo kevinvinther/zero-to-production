b4e93b04b7c94574055dd79018528ef329f5f7ea
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-07-31 15:23:57 +0200

    style: use variable directly in format!
    Clippy complained.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
     let app = Router::new().route("/health_check", get(health_check));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("Listening on http://{}", addr);
+    println!("Listening on http://{addr}");
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
